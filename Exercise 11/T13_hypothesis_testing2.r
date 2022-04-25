61845ea72ca29578d39480ae4389f2d5127ae69e
Michal <michal.beres@vsb.cz>
2022-04-25 14:01:35 +0200

    Added exercise 11

--- Exercise 11/T13_hypothesis_testing2.r
+++ Exercise 11/T13_hypothesis_testing2.r
@@ -7,18 +7,18 @@
 # Use CTRL + SHIFT + O to display the contents of the script 
 # Use CTRL + ENTER to run commands on a single line 
 
-#  Overview of IO tests/constructions ####
+#  Overview of confidence intervals and hypothesis tests and their constructions ####
 #  
-# two-sample data
+# * Two columns of data - pairs/independent ####
 #  
-# - Paired data indicates data that are taken to two measurements of the same entities
-# ->data columns are dependent.
-#  
-# - If they are independent, this is a two-sample test.
-#  
-# - For paired data, we calculate the difference between the columns(or another function
-# according to the input) and use one-sample tests for this difference.
-#  
+# - Paired data indicates data that are taken as two measurements of the same entities
+# -> data columns are dependent.
+#      - For paired data, we calculate the difference between the columns(or another
+# function according to the input) and use one-sample tests for this difference.
+# 
+# - If there is no dependency between values in the two columns the data are
+# independent. 
+#     - Two-sample test is needed
 
 
 # ** Examples of paired data: ####
@@ -26,57 +26,49 @@
 # - measuring bulbs at two different temperatures(if each piece is measured twice - at
 # temperature 1 and temperature 2)
 #  
-# - be careful here, it can happen that the tests are eg destructive and it is not
+# - be careful here, it can happen that the tests are eg. destructive and it is not
 # possible to measure twice the same entity(product). Then we would consider two
-# independent selections, each for one type of measurement ->independent data columns
-# ->two-sample tests
+# independent selections, each for one type of measurement -> independent data columns
+# -> two-sample tests
 #  
 # - measurement of the patient's blood values before and after drug administration
 #  
 # - again pay attention to, for example, drug testing in two groups(placebo/real drug)
-# ->two independent groups ->two-sample tests
-#  
+# ->two independent groups -> two-sample tests
 
 
-# * In general for two-sample tests/IO ####
+# * In general for two-sample tests/CI ####
 #  
-# - the test is always tied to the appropriate IO ->same conditions of use
+# - the test is always tied to the appropriate CI -> same conditions of use
 #  
 # - if the test has conditions of use(eg: normality of data, symmetry of data) then this
-# condition must be met **both files**, if at least one does not meet, we consider the
-# assumption to be broken
+# condition must be met **for both data columns**, if at least one does not meet, we
+# consider the assumption to be broken
 #  
 # - one of the very important assumptions is data independence
 #  
 # - eg: measurement of products of manufacturer A and products of manufacturer B - here
 # it is reasonable to assume that the products of manufacturer A are separate entities
 # from the products of manufacturer B
-#  
 
 
 # * Two-sample tests/IO - difference of position measures ####
 #  
 
 
-# we make test data - so it can be used everywhere
+# we create test data
 data1 = rnorm(n = 30, mean = 105, sd = 10)
 data2 = rnorm(n = 30, mean = 100, sd = 10)
 boxplot(data1,data2)
 
 # ** Two-sample Student's t-test ####
 #  
-# - Tests/estimates mean difference: $H_0: \mu_{1} - \mu_{2} = a$
-#  
+# - Tests/estimates difference of means: $H_0: \mu_{1} - \mu_{2} = a$
 # - requirements:
-#  
-# - Data normality
-#  
-# - Homoskedasticity(scatter matching)
-#  
-# - independence of selections
-#  
+#     - Data normality
+#     - Homoskedasticity(scatter matching)
+#     - independence of selections
 # - the function must have the parameter var.equal=TRUE
-#  
 
 
 # H0: mu1 - mu2=2
@@ -99,16 +91,11 @@ t.test(x = data1, y = data2, mu = 2, alternative = "less",
 
 # ** Aspin-Welsh test ####
 #  
-# - Tests/estimates the mean difference: $H_0: \mu_{1} - \mu_{2} = a$
-#  
+# - Tests/estimates the difference of means: $H_0: \mu_{1} - \mu_{2} = a$
 # - requirements:
-#  
-# - Data normality
-#  
-# - independence of selections
-#  
+#     - Data normality
+#     - independence of selections
 # - the function must have the parameter var.equal=FALSE
-#  
 
 
 # H0: mu1 - mu2=2
@@ -131,16 +118,11 @@ t.test(x = data1, y = data2, mu = 0, alternative = "less",
 
 # ** Mann-Whitney test ####
 #  
-# - Tests/estimates median difference: $H_0: X_{0.5,1} - X_{0.5,2} = a$
-#  
+# - Tests/estimates difference of medians: $H_0: X_{0.5,1} - X_{0.5,2} = a$
 # - requirements:
-#  
-# - independence of selections
-#  
-# -(same type of division)
-#  
-# - requires conf.int=TRUE, to calculate IO
-#  
+#     - independence of selections
+#     - (same shape of the distribution)
+# - requires conf.int=TRUE, to calculate CI
 
 
 # H0: X0.5,1 - X0.5,2=2
@@ -161,18 +143,14 @@ wilcox.test(x = data1, y = data2, mu = 2, alternative = "greater",
 wilcox.test(x = data1, y = data2, mu = 2, alternative = "less",
             conf.level=0.95, conf.int = TRUE)
 
-# * Two-sample tests/IO - proportion of variances ####
+# * Two-sample tests/CI - proportion of variances ####
 #  
 # ** F-test ####
 #  
-# - Tests/estimates the variance ratio: $H_0: \sigma^2_{1} / \sigma^2_{2} = a$
-#  
+# - Tests/estimates the ratio of variances: $H_0: \sigma^2_{1} / \sigma^2_{2} = a$
 # - requirements:
-#  
-# - normality dat
-#  
-# - independence of selections
-#  
+#     - data normality
+#     - independence of selections
 
 
 # H0: sigma1 ^ 2/sigma2 ^ 2=1
@@ -196,15 +174,10 @@ var.test(x = data1, y = data2, ratio = 1, alternative = "less",
 # ** Levene's test ####
 #  
 # - Tests equality of variances: $H_0: \sigma^2_{1} = \sigma^2_{2}$!
-#  
 # - requirements:
-#  
-# - independence of selections
-#  
-# - requires data in standard data format
-#  
+#     - independence of selections
+# - requires data in standard data format 
 # - leveneTest function in the car package
-#  
 
 
 # we produce data in a standard data format
@@ -229,18 +202,14 @@ head(data)
 
 car::leveneTest(data$data ~ data$typ)
 
-# * Two-sample tests/IO - probability difference ####
+# * Two-sample tests/CI - difference of probabilities ####
 #  
-# ** Homogeneity test of two binomial distributions ####
+# ** Test of parameter of two binomial distributions ####
 #  
-# - Tests the match/estimates the probability difference: $H_0: \pi_{1} - \pi_{2} = 0$
-#  
+# - Tests if the probability matches: $H_0: \pi_{1} - \pi_{2} = 0$
 # - requirements:
-#  
-# - sufficient selection size: $n_i>\frac{9}{p_i(1-p_i)}$
-#  
-# - independence of selections
-#  
+#     - sufficient selection size: $n_i>\frac{9}{p_i(1-p_i)}$
+#     - independence of selections
 
 
 # we will produce suitable data
@@ -280,126 +249,99 @@ prop.test(x = c(x1, x2), n = c(n1, n2), alternative="less",
           conf.level=0.95)
 
 #  Examples ####
+
+
+library(dplyr)
+library(rstatix)
+
 #  
-# * Příkald 1. ####
+# * Example 1. ####
 #  
 # Data in the cholesterol2.xls file indicate the blood cholesterol level of men of two
 # different age groups(20-30 years and 40-50 years). Verify at the significance level
-# 0.05 hypothesis that the cholesterol level in the blood of older men does not differ
-# from the cholesterol level in the blood of younger men.
-#  
+# 0.05 the hypothesis that the cholesterol level in the blood of older men does not
+# differ from the cholesterol level in the blood of younger men.
 
 
 # Load data
 chol = readxl::read_excel("data/testy_dvouvyberove.xlsx",
                   sheet = "cholesterol2",
                   skip = 1)   
-colnames(chol)=c("mladsi","starsi")
+colnames(chol)=c("young","old")
 head(chol)
 
 # Convert to standard data format
 chol.s = stack(chol)
 chol.s = na.omit(chol.s)
-colnames(chol.s) = c ("hodnoty","skupina")
+colnames(chol.s) = c ("values","group")
 head(chol.s)
 
 # Exploratory analysis
-boxplot(chol.s$hodnoty ~ chol.s$skupina)
+boxplot(chol.s$values ~ chol.s$group)
 
-# Elimination of remote observations:
-chol.s$hodnoty.bez = chol.s$hodnoty
+# Elimination of outliars:
+chol.s$id = seq(1,length(chol.s$values))
 
-pom = boxplot(chol.s$hodnoty[chol.s$skupina == "mladsi"], plot = FALSE)
-chol.s$hodnoty.bez[chol.s$skupina == "mladsi" & 
-                   chol.s$hodnoty %in% pom$out] = NA
+outliars = chol.s %>% group_by(group) %>% identify_outliers(values)
+outliars
 
-pom = boxplot(chol.s$hodnoty[chol.s$skupina == "starsi"], plot = FALSE)
-chol.s$hodnoty.bez[chol.s$skupina == "starsi" & 
-                   chol.s$hodnoty %in% pom$out] = NA
+chol.s$values_cleared = ifelse(chol.s$id %in% outliars$id, NA, chol.s$values)
 
-boxplot(chol.s$hodnoty.bez~chol.s$skupina)
+boxplot(chol.s$values_cleared~chol.s$group)
 
-# be careful in the data we have NA and we have to reckon with it !!!
-# eg for length determination)
+# be careful in the data we have NA
+# eg for length determination
 
-
-library(dplyr)
-
-chol.s %>%  group_by(skupina) %>% 
-            summarise(count = sum(!is.na(hodnoty.bez)),
-                      prumer = mean(hodnoty.bez, na.rm = TRUE), 
-                      smer.odch = sd(hodnoty.bez, na.rm = TRUE))
+chol.s %>%  group_by(group) %>% 
+            summarise(count = sum(!is.na(values_cleared)),
+                      mean = mean(values_cleared, na.rm = TRUE), 
+                      std = sd(values_cleared, na.rm = TRUE))
 
 # rounding ->3 valid digits ->according to sd to thousands
 
 
-# **Mean/median compliance test**
-#  
+# **Difference of Mean/median test**
 
 
 # Verification of normality
-chol.s %>%  group_by(skupina) %>% 
-            summarise(norm.pval = shapiro.test(hodnoty.bez)$p.value)
+chol.s %>%  group_by(group) %>% 
+            summarise(norm.pval = shapiro.test(values_cleared)$p.value)
 
-# normality in hl. significance 0.05 OK
-
-
-# Verification of variance agreement
-
-# Exploratory
-rozptyly = chol.s %>%  group_by(skupina) %>% 
-            summarise(rozptyl = sd(hodnoty.bez, na.rm = TRUE)^2)
-rozptyly
-max(rozptyly$rozptyl)/min(rozptyly$rozptyl)
-
-# exploratory assessment: the ratio of the largest to the smallest is>than 2
-# ->I do not assume variance agreement
+# normality at significance 0.05 OK
 
 
 # Exactly by F-test
 
-# H0: sigma.starsi=sigma.mladsi
-# Ha: sigma.starsi<>sigma.mladsi
+# H0: sigma.old=sigma.young
+# Ha: sigma.old<>sigma.young
 
 # I select the required data
-starsi.bez = chol.s$hodnoty.bez[chol.s$skupina == "starsi"]
-mladsi.bez = chol.s$hodnoty.bez[chol.s$skupina == "mladsi"]
+young = chol.s$values_cleared[chol.s$group == "young"]
+old = chol.s$values_cleared[chol.s$group == "old"]
 
-var.test(x = starsi.bez, y = mladsi.bez, ratio = 1, conf.level=0.95)
+var.test(x = young, y = old, ratio = 1, conf.level=0.95)
 
-# In hl. significance 0.05 we reject the assumption of agreement of variances
-# The observed discrepancy between the variances can be at the significance level of 0.05
+# at. significance 0.05 we reject the assumption of same variances
+# The observed discrepancy between the variances is significant at the significance level of 0.05
 # Mark as statistically significant.
 
 
-# Verification of agreement of mean values(Aspin-Welch test)
+# Verification of same mean values(Aspin-Welch test)
 
-# H0: mu.starsi - mu.mladsi=0
-# Ha: mu.starsi - mu.mladsi!=0
+# H0: mu.old - mu.young=0
+# Ha: mu.old - mu.young!=0
 
-t.test(x = starsi.bez, y = mladsi.bez, mu = 0, 
+t.test(x = old, y = young, mu = 0, 
        alternative = "two.sided", var.equal=FALSE, conf.level=0.95)
 
 # in hl. significance 0.05 we reject H0->there is a stat. significant difference.
 
 
-# H0: mu.starsi=mu.mladsi(mu.starsi - mu.mladsi=0)
-# Ha: mu.starsi>mu.mladsi(mu.starsi - mu.mladsi>0)
+# H0: mu.old=mu.young
+# Ha: mu.old>mu.young
 
-t.test(x = starsi.bez, y = mladsi.bez, mu = 0, alternative = "greater",
+t.test(x = old, y = young, mu = 0, alternative = "greater",
        var.equal = FALSE, conf.level = 0.95)
-
-# At the significance level of 0.05, we reject the assumption of medium agreement
-# Cholesterol levels in groups of younger and older men in favor
-# Alternatives that older men have higher mean cholesterol levels
-# than men younger
-# According to the results of the sample survey, we expect a medium content
-# cholesterol in the blood of scared men will be about 0.524 mmol/l higher than
-# medium chol. in younger men. According to 95% left intervention.
-# to estimate the difference we expect the mean cholesterol content at
-# older men at least 0.457 mmol/l higher than the mean value
-# Cholesterol in younger men.
-
 
 # * Example 2. ####
 #  
@@ -420,52 +362,52 @@ head(deprese)
 # Conversion to standard data format
 deprese.s = stack(deprese)
 deprese.s = na.omit(deprese.s)
-colnames(deprese.s) = c ("hodnoty","skupina")
+colnames(deprese.s) = c ("values","group")
 
 head(deprese.s)
 
 # Exploratory analysis
-boxplot(deprese.s$hodnoty~deprese.s$skupina)
+boxplot(deprese.s$values~deprese.s$group)
 
-# Data does not contain remote observations.
+# Data does not contain outliers
 
 
 library(dplyr)
 
-deprese.s %>%  group_by(skupina) %>% 
-                summarise(count = length(hodnoty),
-                          prumer = mean(hodnoty), 
-                          smer.odch = sd(hodnoty))
+deprese.s %>%  group_by(group) %>% 
+                summarise(count = length(values),
+                          mean = mean(values), 
+                          std = sd(values))
 
-# rounding ->3 valid digits ->according to sd per unit
+# rounding ->3 valid digits ->according to sd
 
 
-# **Mean/median compliance test**
+# **Diference of Mean/median test**
 #  
 
 
 # Normality verification
-# We assume the assumption of normality by the Shapir - Wilkov test.
-deprese.s %>%  group_by(skupina) %>% 
-                summarise(norm.pval = shapiro.test(hodnoty)$p.value)
+# We assume the assumption of normality by the Shapir - Wilk test.
+deprese.s %>%  group_by(group) %>% 
+                summarise(norm.pval = shapiro.test(values)$p.value)
 
 
-# In hl. significance of 0.05, we reject the assumption of normality
+# at significance 0.05, we reject the assumption of normality
 
 
 # at least as a guide, we check the similarity of the distribution
 
 # we choose data for easier processing
 
-neuro = deprese.s$hodnoty[deprese.s$skupina == "neuro"]
-endo = deprese.s$hodnoty[deprese.s$skupina == "endo"]
+neuro = deprese.s$values[deprese.s$group == "neuro"]
+endo = deprese.s$values[deprese.s$group == "endo"]
 
 
 par(mfrow = c(1,2))
 hist(neuro)
 hist(endo)
 
-# Verification of median compliance(Mann-Whitney test)
+# Difference of median (Mann-Whitney test)
 
 # According to the histograms, we assume that the data have the same type of distribution.
 
@@ -475,7 +417,7 @@ hist(endo)
 wilcox.test(x = neuro, y = endo, mu = 0, alternative = "two.sided",
             conf.level=0.95, conf.int = TRUE)
 
-# in hl. significance 0.05 we reject H0->there is a stat. significant difference
+# at significance 0.05 we reject H0->there is a stat. significant difference
 
 
 # H0: med.neuro=med.endo(med.neuro - med.endo=0)
@@ -484,21 +426,9 @@ wilcox.test(x = neuro, y = endo, mu = 0, alternative = "two.sided",
 wilcox.test(x = neuro, y = endo, mu = 0, alternative = "greater",
             conf.level=0.95, conf.int = TRUE)
 
-# At the significance level of 0.05 we reject hyp. on the agreement of median times to
-# disease remission for both groups of patients in favor of the alternative
-
-# Median remission time is statistically significant in patients with neurotic depression
-# significantly greater than in patients with endogenous depression.
-
-# The remission time of patients with neurotic depression is about 191 days longer
-# than in patients with endogenous depression. According to 95% left
-# interval estimation is expected for patients with neuro. depression have
-# at least 168 days longer remission than endo patients. depression.
-
-
 # * Example 3. ####
 #  
-# We monitor urine osmolality at the inpatient station at 08:00 and 11:00 at 16 men.
+# We monitor urine osmolality at the patient station at 08:00 and 11:00 for 16 men.
 # Based on the results in the osmolality.xls file, verify that the osmolality has
 # increased statistically significantly.
 #  
@@ -512,52 +442,43 @@ colnames(osmolalita)=c("o8","o11")
 head(osmolalita)
 
 # Calculation of osmolality increase
-osmolalita$narust = osmolalita$o11 -  osmolalita$o8
+osmolalita$increase = osmolalita$o11 -  osmolalita$o8
 
 # Exploratory analysis
 par(mfrow = c(1,1))
-boxplot(osmolalita$narust)
+boxplot(osmolalita$increase)
 
-# Data contains remote observations.
+# Data contains outliars
 
 
-# Remove outliers
-pom = boxplot(osmolalita$narust,plot = F)
+# Elimination of outliars:
+osmolalita$id = seq(1,length(osmolalita$increase))
 
-osmolalita$narust.bez = osmolalita$narust
-osmolalita$narust.bez[osmolalita$narust %in% pom$out] = NA
+outliars = osmolalita %>% identify_outliers(increase)
+outliars
+osmolalita$increase_cleared = ifelse(osmolalita$id %in% outliars$id, NA, osmolalita$increase)
 
-boxplot(osmolalita$narust.bez)
+boxplot(osmolalita$increase_cleared)
 
-# Exploratory analysis for data without remote observations
+# Exploratory analysis for data without outliars
 library(dplyr)
 
-osmolalita %>% summarise(count = sum(!is.na(narust.bez)),
-                         prumer = mean(narust.bez, na.rm = TRUE), 
-                         smer.odch = sd(narust.bez, na.rm = TRUE))
+osmolalita %>% summarise(count = sum(!is.na(increase_cleared)),
+                         mean = mean(increase_cleared, na.rm = TRUE), 
+                         std = sd(increase_cleared, na.rm = TRUE))
 
 # rounding ->2 valid digits ->according to sd per unit
 
 
 # Verification of normality
 # The presumption of normality is verified by the Shapir - Wilk test.
-shapiro.test(osmolalita$narust.bez)
-# In hl. significance of 0.05, the assumption of normality cannot be rejected
-# Shapir-Wilk test, W=0.949, p-value=0.545).
-
+shapiro.test(osmolalita$increase_cleared)
 
 # Paired t-test
-# H0: mu.narust=0 mm
-# Ha: mu.narust>0 mm
+# H0: mu.increase=0 mm
+# Ha: mu.increase>0 mm
 
-t.test(osmolalita$narust.bez, mu = 0, alternative = "greater")
-
-# According to a sample survey, urine osmolality can be expected to increase
-# between 8 and 11 o'clock increases by about 24 mmol/kg. According to the 95% interval estimate
-# osmolality can be expected to increase by at least 10 mmol/kg).
-# At a significance level of 0.05, this increase can be described as statistically
-# significant(paired t-test, t=3.1, df=13, p-value=0.005).
-
+t.test(osmolalita$increase_cleared, mu = 0, alternative = "greater")
 
 # * Example 4. ####
 #  
@@ -587,12 +508,6 @@ p.PP
 9/(p.MM*(1-p.MM))
 9/(p.PP*(1-p.PP))
 
-# Furthermore, for both companies we assume that n/N<0.05, ie that the given
-# population(components) has a range of at least 20 * n, ie 20 * 200(4,000), resp. 20 *
-# 150(3,000) components, which is probably a fairly realistic assumption.
-#  
-
-
 # Pearson's X2 test
 # H0: pi.PP=pi.MM
 # Ha: pi.PP>pi.MM
@@ -600,7 +515,7 @@ p.PP
 prop.test(x = c(x.PP,x.MM),n = c(n.PP,n.MM), alternative = "greater",
           conf.level = 0.95)
 
-# Due to the p-value>hl. significance 0.05 we do not reject H0 - ie assumption.
+# at significance 0.05 we do not reject H0 - ie assumption.
 # identical error rates. Therefore, it cannot be said that MM has better production.
 
 
@@ -621,5 +536,8 @@ prop.test(x = c(x.PP,x.MM),n = c(n.PP,n.MM), alternative = "two.sided",
 # -0.036; 1,000) the observed difference in production quality can be described as
 # not statistically significant. We can reach the same conclusions on the basis of
 # Pearson's right-hand test
+prop.test(x = c(x.PP,x.MM),n = c(n.PP,n.MM), alternative = "two.sided",
+          conf.level = 0.95)
+
 
 
